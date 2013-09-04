b9d5bfd9af2c0c04d732b84b2d01791b1cd72aa1
Alexander Malmberg <alexander@malmberg.org>
2013-09-05 00:05:55 +0100

    Add basic, kinda broken collision detection.

--- level.py
+++ level.py
@@ -1,3 +1,4 @@
+import math
 import numpy
 import pygame
 
@@ -12,14 +13,69 @@ class Level(object):
     # TODO(alex): separate collision map
     # TODO(alex): or just build a coarse bsp tree of the geometry?
     self.collisionmap = self.heightmap
+    self.collisionscale = [0.5, 0.5]
 
     # texture(s)
 
   def Render(self):
     pass
 
+  def CollisionMap(self, x, y):
+    if x < 0 or y < 0:
+      return True
+    if x >= self.collisionmap.shape[1] or y >= self.collisionmap.shape[0]:
+      return True
+    return bool(self.collisionmap[y][x])
+
   def Move(self, p, v):
-    r = p
-    r[0] += v[0]
-    r[1] += v[1]
-    return r
+    p = p[:]
+    p[0] *= self.collisionscale[0]
+    p[1] *= self.collisionscale[1]
+    v = v[:]
+    v[0] *= self.collisionscale[0]
+    v[1] *= self.collisionscale[1]
+    stop = 1.0
+
+    # Check x-crossings.
+    if v[0] > 0:
+      start = int(math.ceil(p[0]))
+      end = int(p[0] + v[0]) + 1
+      d = 1
+      o = 0
+    else:
+      start = int(math.floor(p[0]))
+      end = int(p[0] + v[0])
+      d = -1
+      o = -1
+    for x in xrange(start, end, d):
+      t = (x - p[0]) / v[0]
+      y = int(p[1] + t * v[1])
+      if self.CollisionMap(x + o, y):
+        stop = t
+        break
+
+    # Check y-crossings.
+    if v[1] > 0:
+      start = int(math.ceil(p[1]))
+      end = int(p[1] + v[1]) + 1
+      d = 1
+      o = 0
+    else:
+      start = int(math.floor(p[1]))
+      end = int(p[1] + v[1])
+      d = -1
+      o = -1
+    for y in xrange(start, end, d):
+      t = (y - p[1]) / v[1]
+      if t > stop:
+        break
+      x = int(p[0] + t * v[0])
+      if self.CollisionMap(x, y + o):
+        stop = t
+        break
+
+    p[0] += v[0] * stop
+    p[1] += v[1] * stop
+    p[0] /= self.collisionscale[0]
+    p[1] /= self.collisionscale[1]
+    return p
