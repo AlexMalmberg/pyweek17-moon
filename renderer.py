3843a9dcb7bf9a667298489735b05e7d083efbf0
Alexander Malmberg <alexander@malmberg.org>
2013-09-06 21:17:56 +0100

    Add a basic moon shadow shader.

--- renderer.py
+++ renderer.py
@@ -1,6 +1,79 @@
 import pygame
 from OpenGL.GL import *
 
+
+def CompileShader(src, kind):
+  shader = glCreateShader(kind)
+  glShaderSource(shader, [src])
+  glCompileShader(shader)
+  if not glGetShaderiv(shader, GL_COMPILE_STATUS):
+    print 'failed to compile shader: %s' % glGetShaderInfoLog(shader)
+  return shader
+
+
+def CompileProgram(vsrc, fsrc):
+  program = glCreateProgram()
+
+  if vsrc:
+    vshader = CompileShader(vsrc, GL_VERTEX_SHADER)
+    glAttachShader(program, vshader)
+    glDeleteShader(vshader)
+  if fsrc:
+    fshader = CompileShader(fsrc, GL_FRAGMENT_SHADER)
+    glAttachShader(program, fshader)
+    glDeleteShader(fshader)
+
+  glLinkProgram(program)
+  glValidateProgram(program)
+  return program
+
+
+moonlight_vshader = """
+#version 110
+
+varying vec4 position;
+varying vec2 texcoord;
+
+void main() {
+  gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+  position = gl_Vertex;
+  texcoord = gl_MultiTexCoord0.st;
+}
+"""
+
+moonlight_fshader = """
+#version 110
+
+varying vec4 position;
+varying vec2 texcoord;
+
+uniform sampler2D tex;
+
+uniform vec4 ambient;
+
+uniform float moon1_blend;
+uniform vec4 moon1_color;
+uniform sampler2D moon1_1, moon1_2;
+
+void main() {
+  vec4 color = texture2D(tex, texcoord);
+  vec4 light_pos = position / 2048.;
+
+  vec4 light = ambient;
+
+  float light1 = texture2D(moon1_1, light_pos.xy).a;
+  light1 = 1. - clamp(light1 * 20., 0., 1.);
+
+  float light2 = texture2D(moon1_2, light_pos.xy).a;
+  light2 = 1. - clamp(light2 * 20., 0., 1.);
+
+  light += moon1_color * mix(light1, light2, moon1_blend);
+
+  gl_FragColor = color * light;
+}
+"""
+
+
 class Render(object):
   def __init__(self, screen):
     width = screen.get_width()
@@ -14,6 +87,9 @@ class Render(object):
     # of them toavoid problems with trying to make sure we don't leak
     # them as objects get created and destroyed.
     self.lightmap_ids = {}
+
+    self.moonlight_program = CompileProgram(
+      moonlight_vshader, moonlight_fshader)
 
   def SetupProjection(self, x, y, height):
     glMatrixMode(GL_PROJECTION)
@@ -42,8 +118,38 @@ class Render(object):
                  GL_UNSIGNED_BYTE, raw)
     return id
 
-
   def LightmapTextureIds(self, index):
     if index not in self.lightmap_ids:
       self.lightmap_ids[index] = glGenTextures(3)
     return self.lightmap_ids[index]
+
+  def SetMoonlightShader(self, ambient, moons):
+    prg = self.moonlight_program
+    glUseProgram(prg)
+
+    loc = glGetUniformLocation(prg, 'ambient')
+    glUniform4f(loc, *ambient)
+
+    m = moons[0]
+
+    loc = glGetUniformLocation(prg, 'moon1_blend')
+    glUniform1f(loc, m.blend)
+
+    loc = glGetUniformLocation(prg, 'moon1_color')
+    glUniform4f(loc, m.color[0], m.color[1], m.color[2], 1.0)
+
+    loc = glGetUniformLocation(prg, 'moon1_1')
+    glUniform1i(loc, 1)
+    glActiveTexture(GL_TEXTURE1)
+    glBindTexture(GL_TEXTURE_2D,
+                  m.lightmaps[m.active_lightmap].id)
+
+    loc = glGetUniformLocation(prg, 'moon1_2')
+    glUniform1i(loc, 2)
+    glActiveTexture(GL_TEXTURE2)
+    glBindTexture(GL_TEXTURE_2D,
+                  m.lightmaps[m.next_lightmap].id)
+
+    glActiveTexture(GL_TEXTURE0)
+    loc = glGetUniformLocation(prg, 'tex')
+    glUniform1i(loc, 0)
