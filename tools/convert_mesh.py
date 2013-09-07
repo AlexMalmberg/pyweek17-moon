40a70cc465c300934e93981fb9f1f019c707c1ac
Alexander Malmberg <alexander@malmberg.org>
2013-09-07 14:00:03 +0100

    Add support for axis reordering and faces with >3 vertices.

--- tools/convert_mesh.py
+++ tools/convert_mesh.py
@@ -7,23 +7,28 @@ def Normalize(v):
   return (v[0] / d, v[1] / d, v[2] / d)
 
 
-def LoadMesh(filename, scale, offset):
+def LoadMesh(filename, scale, offset, axis_order):
   vertices = []
   faces = []
   normals = []
   tvertices = []
 
+  flip = axis_order in ((0, 2, 1), (1, 0, 2), (2, 1, 0))
+
+  def FixVert(v):
+    return [v[axis_order[0]], v[axis_order[1]], v[axis_order[2]]]
+
   for line in file(filename):
     if line[0] == '#':
       continue
     if line.startswith('v '):
-      vert = map(float, line.split()[1:4])
+      vert = FixVert(map(float, line.split()[1:4]))
       for i in xrange(3):
         vert[i] = vert[i] * scale[i] + offset[i]
       vertices.append(vert)
       continue
     if line.startswith('vn '):
-      vert = map(float, line.split()[1:4])
+      vert = FixVert(map(float, line.split()[1:4]))
       for i in xrange(3):
         vert[i] = vert[i] * scale[i]
       vert = Normalize(vert)
@@ -32,6 +37,7 @@ def LoadMesh(filename, scale, offset):
       vert = map(float, line.split()[1:3])
       tvertices.append(vert)
     if line.startswith('f '):
+
       def ParseVert(s):
         temp = s.split('/')
         while len(temp) < 3:
@@ -42,8 +48,13 @@ def LoadMesh(filename, scale, offset):
           except ValueError:
             temp[i] = 1
         return tuple(temp)
-      vtps = map(ParseVert, line.split()[1:4])
-      faces.append(vtps)
+
+      raw_vtps = map(ParseVert, line.split()[1:])
+      for i in xrange(len(raw_vtps) - 2):
+        faces.append((raw_vtps[0], raw_vtps[i + 1], raw_vtps[i + 2]))
+
+      #vtps = map(ParseVert, line.split()[1:4])
+      #faces.append(vtps)
       continue
 
   print ('%i vertices, %i normals, %i tvertices, %i faces'
@@ -73,6 +84,8 @@ def LoadMesh(filename, scale, offset):
   gl_indices = []
   num_v = 0
   for face in faces:
+    if flip:
+      face = list(reversed(face))
     for vtp in face:
       if vtp not in vtp_seen:
         v = vertices[vtp[0] - 1]
@@ -106,11 +119,16 @@ def main():
   else:
     scale = (1, 1, 1)
   if len(sys.argv) > 3:
-    offset = tuple(map(float, sys.argv[2].split(',')))
+    offset = tuple(map(float, sys.argv[3].split(',')))
   else:
     offset = (0, 0, 0)
 
-  LoadMesh(filename, scale, offset)
+  if len(sys.argv) > 4:
+    axis_order = tuple(map(int, sys.argv[4].split(',')))
+  else:
+    axis_order = (0, 1, 2)
+
+  LoadMesh(filename, scale, offset, axis_order)
 
 
 if __name__ == '__main__':
