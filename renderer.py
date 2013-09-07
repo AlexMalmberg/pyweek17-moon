21f93167617411d5dbafb3a993fdb9ed98514d7b
Alexander Malmberg <alexander@malmberg.org>
2013-09-07 17:03:23 +0100

    Transform normals.

--- renderer.py
+++ renderer.py
@@ -45,7 +45,7 @@ void main() {
   gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
   position = gl_ModelViewMatrix * gl_Vertex;
   texcoord = gl_MultiTexCoord0.st;
-  normal = gl_Normal;
+  normal = normalize(gl_NormalMatrix * gl_Normal);
 }
 """
 
