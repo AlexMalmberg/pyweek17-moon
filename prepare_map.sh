02b33fe6253ecd061b3d622e4140c18c6007d005
Alexander Malmberg <alexander@malmberg.org>
2013-09-07 17:35:40 +0100

    Try to force an 8-bit png out of pnmtopng.

--- prepare_map.sh
+++ prepare_map.sh
@@ -6,6 +6,6 @@ if [[ -z $1 ]] ; then
 fi
 
 python make_heightmap.py $1
-pnmflip -tb $1/height.pnm | pnmtopng -compression 9 > $1/height.png
+pnmflip -tb $1/height.pnm | pnmtopng -compression 9 -force > $1/height.png
 pnmflip -tb $1/height.pnm | python tools/make_collisionmap.py \
     | pnmtopng -compression 9 > $1/collision.png
